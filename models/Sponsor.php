1ba094eb8f02bd18c1155601d9a255f0b4b323dc
asuProject <33201495+asuProject@users.noreply.github.com>
2017-10-31 03:07:57 +0200

    Initial commit

---
+++ models/Sponsor.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Sponsor extends Model
+{
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'sponsors';
+
+	/**
+	 * The attributes that are mass assignable.
+	 *
+	 * @var array
+	 */
+	protected $fillable = ['country_id', 'town_id', 'name', 'email', 'phone', 'website', 'comments'];
+
+	/**
+	 * The attributes excluded from the model's JSON form.
+	 *
+	 * @var array
+	 */
+	protected $hidden = [];
+
+	public function Country()
+	{
+		return $this->belongsTo('App\Country');
+	}
+
+	public function Town()
+	{
+		return $this->belongsTo('App\Town');
+	}
+}
---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/**********************************************************************************************/
+
+
+/**********************/
+/**
+ * Conference Type routes
+ */
+Route::resource('conferenceType', 'ConferenceTypesController');
+/**********************/
+/**********************/
+/**
+ * Country & Towns routes
+ */
+Route::resource('country/country', 'Country\CountryController', ['names' => 'country.country']);
+Route::post('/town/ajax_get','Country\TownController@')->name('town.ajax.get');
+Route::resource('country/town', 'Country\TownController', ['names' => 'country.town']);
+/**********************/
+/**********************/
+/**
+ * Sponsor routes
+ */
+Route::resource('sponsor', 'SponsorController', ['names' => 'sponsor']);
+/**********************/
